fe5992b49c51fbce50f78ddec1e7cc291993eca1
NguyenNhatCP <ducnhat1708@gmail.com>
2025-10-03 14:43:24 +0700

    edit index

--- alert.js
+++ alert.js
@@ -27,7 +27,6 @@ export async function runAlert() {
   try {
     const data = await getFearGreedIndex();
     const value = data?.value;
-    //const status = data?.value_text;
 
     console.log("Fear & Greed:", value);
 
@@ -35,10 +34,9 @@ export async function runAlert() {
       await sendTelegram(`⚠️ Cảnh báo: Fear & Greed Index = ${value}`);
     } else if (value > 80) {
       await sendTelegram(`🚀 Thị trường quá tham lam! Index = ${value}`);
+    } else {
+      await sendTelegram(`ℹ️ Fear & Greed Index: ${value}`);
     }
-    // } else {
-    //   await sendTelegram(`ℹ️ Fear & Greed Index: ${value} (${status})`);
-    // }
   } catch (e) {
     console.error("Error:", e.message);
   }
